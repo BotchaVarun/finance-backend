771c1e754bd38d35ed97a6f11cadc07b0a2f2366
BotchaVarun <varunbotcha@gmail.com>
2025-04-08 22:10:49 +0530

    patch

--- config/db.js
+++ config/db.js
@@ -4,7 +4,10 @@ require('dotenv').config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://varunbotcha:varun123@cluster0.afzwir6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect("mongodb+srv://varunbotcha:varun123@cluster0.afzwir6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+  });
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err.message);
